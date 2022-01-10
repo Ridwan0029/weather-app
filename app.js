82df43f5d104f691a6524ccc90d5855c83f713c8
Ridwan <ridwanraji4@gmail.com>
2022-01-10 05:26:28 +0100

    callback errors

--- app.js
+++ app.js
@@ -21,8 +21,14 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDtGl6QfpUIOyPP-uWLXEzOuOTJXg_h5UE&address= ${encodedAddress}`,
     json: true
 }, (error, response, body) => {
+    if(error) {
+        console.log('unable to connect to Google');
+    } else if(body.status === 'ZERO_RESULTS') {
+        console.log('unable to find address');
+    } else if (body.status === 'OK') {
         // console.log(JSON.stringify(body, undefined, 2));
         console.log(`Address: ${body.results[0].formatted_address}`);
         console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
         console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    }
 }) 
