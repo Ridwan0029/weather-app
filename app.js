50e7d3e5d3a9503434b60d6bf3e907403df2f808
Ridwan <ridwanraji4@gmail.com>
2022-01-10 05:24:56 +0100

    Encoding user input

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+// console.log(argv);
+let encodedAddress = encodeURIComponent(argv.a)
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDtGl6QfpUIOyPP-uWLXEzOuOTJXg_h5UE&address=20%20Hospital%20Roadd%20Ajegunle%20Osogbo',
+    // url: 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDtGl6QfpUIOyPP-uWLXEzOuOTJXg_h5UE&address=' + encodedAddress,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDtGl6QfpUIOyPP-uWLXEzOuOTJXg_h5UE&address= ${encodedAddress}`,
     json: true
 }, (error, response, body) => {
-    // console.log(JSON.stringify(body, undefined, 2));
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+        // console.log(JSON.stringify(body, undefined, 2));
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 }) 
