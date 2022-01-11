28dd08e5b72f8ee94b810cba3ac8b61256af2340
Ridwan <ridwanraji4@gmail.com>
2022-01-11 15:49:14 +0100

    refactor - separate request from app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
     .options({
@@ -14,21 +14,4 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 // console.log(argv);
-let encodedAddress = encodeURIComponent(argv.a)
-
-request({
-    // url: 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDtGl6QfpUIOyPP-uWLXEzOuOTJXg_h5UE&address=' + encodedAddress,
-    url: `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDtGl6QfpUIOyPP-uWLXEzOuOTJXg_h5UE&address= ${encodedAddress}`,
-    json: true
-}, (error, response, body) => {
-    if(error) {
-        console.log('unable to connect to Google');
-    } else if(body.status === 'ZERO_RESULTS') {
-        console.log('unable to find address');
-    } else if (body.status === 'OK') {
-        // console.log(JSON.stringify(body, undefined, 2));
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-    }
-}) 
+geocode.geocodeAddress(argv.a)
