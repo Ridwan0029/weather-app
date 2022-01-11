909ae4c764b26d69c029c941e866687d651d8500
Ridwan <ridwanraji4@gmail.com>
2022-01-11 16:03:48 +0100

    abstracting callbacks

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-let geocodeAddress = (address) => {
+let geocodeAddress = (address, callback) => {
     let encodedAddress = encodeURIComponent(address)
 
     request({
@@ -9,14 +9,16 @@ let geocodeAddress = (address) => {
         json: true
     }, (error, response, body) => {
         if(error) {
-            console.log('unable to connect to Google');
+            callback('unable to connect to Google Servers');
         } else if(body.status === 'ZERO_RESULTS') {
-            console.log('unable to find address');
+            callback('unable to find address');
         } else if (body.status === 'OK') {
-            // console.log(JSON.stringify(body, undefined, 2));
-            console.log(`Address: ${body.results[0].formatted_address}`);
-            console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-            console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+            callback(undefined, {
+                // console.log(JSON.stringify(body, undefined, 2));
+                Address: body.results[0].formatted_address,
+                Latitude: body.results[0].geometry.location.lat,
+                Longitude: body.results[0].geometry.location.lng
+            });
         }
     }) 
 }
